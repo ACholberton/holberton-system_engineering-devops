e910c2e6c975d00264a8e9d917317531584f85ff
ACholberton <1103@holbertonschool.com>
2020-05-08 01:36:07 +0000

    created a function that lists the first 10 hot posts of a sub reddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+ #!/usr/bin/python3
+""" function that queries the Reddit API and prints the titles of the firs
+10 hot posts listed for a given subreddit."""
+
+import requests
+
+
+def top_ten(subreddit):
+
+    reddit_url = "https://www.reddit.com/r/" + subreddit + "/hot.json?limit=10"
+    header = {"User-Agent": "Chrome/81.0.4044.129"}
+    req = requests.get(reddit_url, headers=header)
+    reddit = req.json()
+
+    if (req.status_code == 200):
+        """checks if the response status is ok"""
+        hot_post = reddit.get("data").get("children")
+        for post in hot_post:
+            print(post.get("data").get("title"))
+    else:
+        print(None)
