055d3037198356b572d0f73062bf66dcf434dc34
ACholberton <1103@holbertonschool.com>
2020-05-21 02:23:33 +0000

    fixed typo and added description

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
-exec { 'replace':
+# Puppet script initiallizes a fix to the error in the wp-settings.php file
+exec { 'replace wrong syntax':
   command => "sed -i -e 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
   path    => '/bin',
 }
